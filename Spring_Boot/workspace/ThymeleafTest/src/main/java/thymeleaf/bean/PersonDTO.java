67593b269beffc7f33a414aa5f2c36dd6998a005
KimSeongChan97 <sck2270@gmail.com>
2024-11-05 17:53:00 +0900

    Thymeleaf 기초

---
+++ Spring_Boot/workspace/ThymeleafTest/src/main/java/thymeleaf/bean/PersonDTO.java
@@ -0,0 +1,14 @@
+package thymeleaf.bean;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class PersonDTO {
+
+	private String name;
+	private int age;
+	
+	
+}
